5d306f153a0b61cbb7436a871c362c3e3a36620f
bossman125 <0014114@student.isf.edu.hk>
2026-01-13 15:28:13 +0800

    fix whitescreen

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/discourse/" // 👈 matches your repo name on GitHub
+  base: "/" // 👈 matches your repo name on GitHub
 });
