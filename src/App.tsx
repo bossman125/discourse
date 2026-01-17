0fd33ede9358596099eac471d3182ac9d33d03ad
jh <0014114@student.isf.edu.hk>
2026-01-17 15:55:19 +0800

    fix reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
@@ -10,25 +11,42 @@ import { Join } from './pages/Join';
 import { Contact } from './pages/Contact';
 import { Submit } from './pages/Submit';
 
+function AppContent() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.search.startsWith('?/')) {
+      const path = location.search.slice(2);
+      const newPath = path.replace(/~and~/g, '&');
+      navigate(newPath, { replace: true });
+    }
+  }, [location, navigate]);
+
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/articles/:id" element={<Article />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/submit" element={<Submit />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router basename="/">
-      <div className="flex flex-col min-h-screen bg-white">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/articles" element={<Articles />} />
-            <Route path="/articles/:id" element={<Article />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/submit" element={<Submit />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
