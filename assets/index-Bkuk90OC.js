e4e39d07b0e50ad468c63ac842fc95324691280c
jingknows <0014082@student.isf.edu.hk>
2026-04-04 12:08:59 +0800

    Updates

--- assets/index-Bkuk90OC.js
+++ assets/index-Bkuk90OC.js
@@ -293,7 +293,7 @@ author: "Zijing Wu"
 summary: "This article examines Australia’s world‑first ban on social media for children under 16, analysing how the policy aims to reduce negative externalities from social media overuse while evaluating its impacts on the economic well‑being of teenagers, the government, and social media platforms."
 issue: "Spring 2026"
 date: "2025-10-27"
-published: true
+published: True
 subject: "Economics"
 pdfFile: "Zijing1.pdf"
 ---
